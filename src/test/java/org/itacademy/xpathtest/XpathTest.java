6e8ed6fcca4cd6753b84bbf44c801130ac863028
Konstantin <novash.ki@gmail.com>
2023-05-28 15:22:34 +0300

    multithread tests executor method was created

--- src/test/java/org/itacademy/xpathtest/XpathTest.java
+++ src/test/java/org/itacademy/xpathtest/XpathTest.java
@@ -4,57 +4,22 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
 public class XpathTest {
 
-    @DataProvider()
-    public Object[][] page_path() {
-        return new Object[][]{
-                /**
-                 * xpath fast test
-                 * insert the { PAGE URL, XPATH } into the dataprovider
-                */
+//    @DataProvider()
+//    public Object[][] page_path() {
+//        return new Object[][]{
+//                {"https://demoqa.com/automation-practice-form", "//*[@id='react-select-3-input']"},
+//                {"https://demoqa.com/automation-practice-form", "//*[@id='currentAddress']"}
+//        };
+//    }
 
-//                {"https://donerking.by/", "//img[@alt='Пончики с шоколадным кремом']"},
-//                {"https://alexstar.ru/", "//*[@id=\"content\"]/div/div[2]/a[3]"},
-//                {"https://alexstar.ru/smarthome/", "//*[@id=\"content\"]/form/div/div[1]/input"},
-//                {"https://donerking.by/", "//*[@id=\"mainheader\"]/div[1]/div/div[1]/div[1]/div[2]/a"}
-                //*[@id="app"]/div/div/div[2]/div/div[2]/div/div[2]/svg
-//                {"https://demoqa.com", "//*[@id=\"app\"]/div/div/div[2]/div/div[2]/div/div[2]"}
-                //li[@class="pcVideoListItem js-pop videoblock"]
-//                {"https://www.pornhub.com/", "//input[@id=\"searchInput\"]"} // серч бокс
-                //*[contains(text(), 'Categories')]
-//                {"https://www.pornhub.com/", "//*[contains(text(), 'Categories')]"} // категории
-                //*[@id="menu"]//*[contains(text(), 'Доставка')]
-//                {"https://tokiny.by/", "//*[@id=\"menu\"]//*[contains(text(), 'Доставка')]"}
-
-                //*[@id="page-content"]/div[2]/div/div[1]/div[2]/div[5]/a/div[1]
-//                {"https://tokiny.by/", "//*[@id=\"page-content\"]/div[2]/div/div[1]/div[2]/div[5]/a/div[1]"}
-
-                //*[@id="page-content"]/div[2]/div/div[1]/div[2]/div[5]/a
-//                {"https://tokiny.by/", "//div[@style='background-image: url(https://tokiny.by/media/zoo/images/roll_new_f2348ed7712b7496e8af874311a209dc.png);']"}
-//                {"https://tokiny.by/", "//div[contains(@style,'/zoo/images/roll_new_')]"}
-                //*[@id="page-content"]/div[2]/div/div[1]/div[2]/div[5]/a
-
-                //input[@id='firstName']
-//                {"https://demoqa.com/automation-practice-form", "//input[@id='firstName']"}
-
-                //div[@class='subjects-auto-complete__placeholder css-1wa3eu0-placeholder']
-//        class="subjects-auto-complete__control css-yk16xz-control"
-
-//                css-2b097c-container
-//                subjects-auto-complete__control css-yk16xz-control
-//                subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3
-//                subjects-auto-complete__placeholder css-1wa3eu0-placeholder
-//                subjects-auto-complete__input
-//                {"https://demoqa.com/automation-practice-form", "//div[@class='subjects-auto-complete__placeholder css-1wa3eu0-placeholder']"}
-
-//                {"https://demoqa.com/automation-practice-form", "//*[@id='react-select-3-input']"}
-
-                {"https://demoqa.com/automation-practice-form", "//*[@id='currentAddress']"}
-        };
-    }
-
-    @Test(testName = "xpath test", dataProvider = "page_path")
+//    @Test(testName = "xpath test", dataProvider = "page_path")
+    @Test(testName = "xpath test")
     public void xpathTest(String page, String path) {
         System.setProperty("webdriver.chrome.driver", "/home/konstantin/Downloads/chromedriver_linux64 113/chromedriver");
         WebDriver driver = new ChromeDriver();
@@ -89,4 +54,43 @@ public class XpathTest {
         }
 
     }
+
+
+    @Test(testName = "multi test")
+    public void multiTest() {
+
+//        System.out.println("ELEMENT CLASS: ");
+//        String[] ip_array = new String[]{"бургер", "донер", "пончики"};
+
+        String pagex = "https://demoqa.com/automation-practice-form";
+
+//        Object[][] pages_xpaths = new Object[][]{
+//                {"https://demoqa.com/automation-practice-form", "//*[@id='react-select-3-input']"},
+//                {"https://demoqa.com/automation-practice-form", "//*[@id='currentAddress']"}
+//        };
+
+        String[] xpaths = new String[]{
+                "//*[@id='react-select-3-input']",
+                "//*[@id='currentAddress']"
+        };
+
+
+        List<String> listof = List.of(xpaths);
+        List<Boolean> result2 =
+                listof.stream()
+                        .map(item -> CompletableFuture.supplyAsync(() -> testExecutor(pagex, item)))
+                        .collect(Collectors.collectingAndThen(Collectors.toList(), cfs -> cfs.stream().map(CompletableFuture::join)))
+                        .toList();
+        System.out.println("\nRESULT 2 : " + result2);
+
+    }
+
+
+    public Boolean testExecutor(String page, String path) {
+        System.out.println("RUN TEST page: " + page + " xpath: " + path);
+        xpathTest(page, path);
+        return true;
+    }
+
+
 }
